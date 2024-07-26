06ba0ee4e7cef8b6e3f5886df55ac33a8d8a0dea
chhun <chhun@CHHUNCHANDALIS>
2024-07-26 20:15:54 +0700

    test

---
+++ aga_project/aga_project/Controllers/test.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace aga_project.Controllers
+{
+    public class test : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
